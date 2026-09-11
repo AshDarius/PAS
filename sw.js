21ce1137ada4890d5e05937700a7046265f77a75
Ash Darius <321621339+AshDarius@users.noreply.github.com>
2026-09-11 12:31:16 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'audio-control-pwa-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://unpkg.com/lucide@latest'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
